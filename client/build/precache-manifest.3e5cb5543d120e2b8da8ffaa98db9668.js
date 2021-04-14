b9d7f00d22e58af8355e27a2a1359e907c306f5e
Murad Cholukov <muradusa@gmail.com>
2021-04-14 11:52:37 -0700

    pushing build folder

---
+++ client/build/precache-manifest.3e5cb5543d120e2b8da8ffaa98db9668.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ff59299e74fb1ee4fceb41f8ca4b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f97665786145018ed23",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "2c7c122bdb4dd6f2e375",
+    "url": "/static/css/main.4ae35c35.chunk.css"
+  },
+  {
+    "revision": "9f97665786145018ed23",
+    "url": "/static/js/2.7dd5e04d.chunk.js"
+  },
+  {
+    "revision": "0f87e0cfbb145332e9889452e79883f4",
+    "url": "/static/js/2.7dd5e04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7c122bdb4dd6f2e375",
+    "url": "/static/js/main.ed87a37b.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "e085041bcb1796ebe1881654607b0995",
+    "url": "/static/media/Upload Successful.e085041b.png"
+  }
+]);
